ad322be6b6a4c0dfb01fc356a0e7aa1fad9ed07c
James Lai <jameslai@Jamess-MacBook-M2-Pro.local>
2024-01-04 17:27:30 +0000

    update POPS resp distress calculator

--- 03_pops_calculator.R
+++ 03_pops_calculator.R
@@ -1,8 +1,13 @@
 #POPS calculator
 #Paediatric Observation Priority Score (POPS)
 
-syn_paeds_data %>%
-  pops_sats = case_when(sats >=95 ~ 0,
-                        sats %in% c(90:95) ~1,
-                        sats <90 ~2)
-  
+test <- syn_paeds_data %>%
+  mutate(pops_sats = case_when(sats >=95 ~ 0,
+                      sats %in% c(90:95) ~1,
+                      sats <90 ~2),
+         pops_breathing = case_when(resp_distress == "No distress" ~ 0,
+                                    resp_distress == "Mild or moderate recession" ~ 1,
+                                    resp_distress == "Audible grunt or wheeze" ~ 1,
+                                    resp_distress == "Severe recession" ~ 2,
+                                    resp_distress == "Stridor" ~ 2))
+
