b7f84ed77d5adabec3cb03704f2a242625ce5f6b
James Lai <jameslai@Jamess-MacBook-M2-Pro.local>
2024-01-04 17:04:59 +0000

    set packages required for project

---
+++ 01_initialise_pops_pews.R
@@ -0,0 +1,12 @@
+#packages required for POPS PEWS project
+
+#load libraries
+library(MASS)
+library(dplyr)
+library(tidyverse)
+library(writexl)
+
+#snthetic data R packages but require existing dataset:
+#synthpop package
+#tidysynth package
+ 
